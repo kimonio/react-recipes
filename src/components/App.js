ef399ed2381d9556828e59e27c9c38fa014bed66
Antonio Cunanan <antonio.cunanan@gmail.com>
2020-12-21 23:45:46 -0600

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import RecipeList from './RecipeList'
+import '../css/styles.css'
+import { v4 as uuidv4 } from 'uuid'
+
+function App() {
+	const [recipes, setRecipes] = useState(sampleRecipes)
+
+	function handleRecipeAdd() {
+		const newRecipe = {
+			id: uuidv4(),
+			name: 'New',
+			servings: 1,
+			cookTime: '1:00',
+			instructions: 'Instr.',
+			ingredients: [{ id: uuidv4(), name: 'Name', amount: '1 Tbs' }],
+		}
+		setRecipes([...recipes, newRecipe])
+	}
+
+	function handleRecipeDelete(id) {
+		console.log('delete recipe with id ' + id)
+		setRecipes(recipes.filter((recipe) => recipe.id !== id))
+	}
+
+	return (
+		<RecipeList
+			recipes={recipes}
+			handleRecipeAdd={handleRecipeAdd}
+			handleRecipeDelete={handleRecipeDelete}
+		/>
+	)
+}
+
+const sampleRecipes = [
+	{
+		id: 1,
+		name: 'Plain Chicken',
+		servings: 3,
+		cookTime: '1:45',
+		instructions:
+			'1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken',
+		ingredients: [
+			{
+				id: 1,
+				name: 'Chicken',
+				amount: '2 pounds',
+			},
+			{
+				id: 2,
+				name: 'Salt',
+				amount: '1 Tbs',
+			},
+		],
+	},
+	{
+		id: 2,
+		name: 'Plain Pork',
+		servings: 5,
+		cookTime: '0:45',
+		instructions: '1. Put paprika on pork\n2. Put pork in oven\n3. Eat pork',
+		ingredients: [
+			{
+				id: 1,
+				name: 'Pork',
+				amount: '3 pounds',
+			},
+			{
+				id: 2,
+				name: 'Paprika',
+				amount: '2 Tbs',
+			},
+		],
+	},
+]
+
+export default App
---
+++ src/components/RecipeList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+export default function RecipeList(props) {
+	const { recipes, handleRecipeAdd, handleRecipeDelete } = props
+	return (
+		<div className='recipe-list'>
+			<div>
+				{recipes.map((recipe) => (
+					<Recipe
+						key={recipe.id}
+						{...recipe}
+						handleRecipeDelete={handleRecipeDelete}
+					/>
+				))}
+			</div>
+			<div className='recipe-list__add-recipe-btn-container'>
+				<button className='btn btn--primary' onClick={handleRecipeAdd}>
+					Add Recipe
+				</button>
+			</div>
+		</div>
+	)
+}
